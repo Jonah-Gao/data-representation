567b7c8df034a616f81f10e6d1d3e29859d76c81
Jonah Gao <gaoj@stpaulsschool.org.uk>
2024-09-23 20:03:28 +0100

    Update unicode db.py

--- unicode db.py
+++ unicode db.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
+# init
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///unicode_table.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///unicode_table.sqlite3'  # db location
 
 db = SQLAlchemy(app)
 
-
+# database structure
 class Unicode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     char = db.Column(db.String(1), unique=True)
@@ -14,9 +15,9 @@ class Unicode(db.Model):
 
 
 with app.app_context():
-    db.create_all()
+    db.create_all()  # creates database
 
-    for i in range(128):
+    for i in range(128):  # adds unicode characters into database
         new_unicode = Unicode(char=chr(i), unicode=f"U+{str(hex(i)).split('x')[-1].zfill(4).upper()}")
         db.session.add(new_unicode)
         db.session.commit()
@@ -24,7 +25,7 @@ with app.app_context():
 # takes a long time to add all the unicode characters, therefore only adding the ascii characters
 
 with app.app_context():
-    unicode = Unicode.query.filter_by(char='A').first()
+    unicode = Unicode.query.filter_by(char='A').first()  # filter by char
     print(unicode.unicode)
-    char = Unicode.query.filter_by(unicode='U+0041').first()
+    char = Unicode.query.filter_by(unicode='U+0041').first()  # filter by unicode
     print(char.char)
