761d1b055a1aba3a23129cc50001c89d6fa590d7
Jonah Gao <gaoj@stpaulsschool.org.uk>
2024-09-23 20:01:00 +0100

    Update char unicode convertion.py

--- char unicode convertion.py
+++ char unicode convertion.py
@@ -2,14 +2,15 @@ import json
 
 
 def unicode_to_char(unicode):
-    with open('unicode.json', 'r') as f:
+    with open('unicode.json', 'r') as f:  # reads the json file
         unicode_table = json.load(f)
-    return unicode_table[unicode]
+    return unicode_table[unicode]  # fetch value by key
 
 
 def char_to_unicode(char, base=16):
-    with open('unicode_inverted.json', 'r') as f:
+    with open('unicode_inverted.json', 'r') as f:  # reads the json file
         unicode_table_inverted = json.load(f)
+    # fetch value by key
     match base:
         case 16:
             return unicode_table_inverted[char]
